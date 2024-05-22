a85d28bfc97a0df7587cab554bb8156944410d52
Svetlana2510 <148907668+Svetlana2510@users.noreply.github.com>
2024-05-22 12:56:54 +0300

    Update 20.03.py

--- 20.03.py
+++ 20.03.py
@@ -4,7 +4,7 @@ def average(*args):
 summa = 0      #summa, count равны нулю для посчета суммы и количества исел 
 count = 0
 for arg in args:
-if isinstance(arg, (int, float)):
+if isinstance(arg, (int, float)): #проверяет, является ли объект подходящим под свойства
 summa += arg
 count += 1
 if count == 0:
